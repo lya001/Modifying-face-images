f0f2976cf0a376ab14f29fcacd82256a20c63e39
yl4519 <yl4519@ic.ac.uk>
2023-01-07 16:01:04 +0000

    refactor: separate setup and inference

---
+++ gfpgan_setup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Clone repo
+git clone https://github.com/lya001/GFPGAN.git
+cd GFPGAN
+
+# Install dependent packages
+pip install basicsr
+pip install facexlib
+pip install -r requirements.txt
+python setup.py develop
+pip install realesrgan
+
+# Download pre-trained model
+wget https://github.com/TencentARC/GFPGAN/releases/download/v1.3.0/GFPGANv1.3.pth -P experiments/pretrained_models
+
+# cleanup
+cd ..
---
+++ setup.sh
@@ -0,0 +1,2 @@
+sh gfpgan_setup.sh
+sh randr_setup.sh
