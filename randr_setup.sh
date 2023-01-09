afa40211206f6c189bf753438fb96acde0d42d80
yl4519 <yl4519@ic.ac.uk>
2023-01-09 17:05:55 +0000

    fix(randr setup): download large checkpoint/model

--- randr_setup.sh
+++ randr_setup.sh
@@ -1,18 +1,26 @@
 #!/bin/bash
 
 # Clone repo
-https://github.com/lya001/Rotate-and-Render.git
+git clone https://github.com/lya001/Rotate-and-Render.git
 cd Rotate-and-Render
 
 # Install dependent packages
 pip install -r requirements.txt
 git clone https://github.com/lya001/neural_renderer.git
-pip install neural_renderer
+pip install -e neural_renderer
 
 # Download checkpoint and model
-wget https://drive.google.com/file/d/1v31SOrGYueeDi2SxOAUuKWqnglEP0xwA/view?usp=sharing -P 3ddfa/
+cd 3ddfa/
+wget --load-cookies /tmp/cookies.txt "https://drive.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1v31SOrGYueeDi2SxOAUuKWqnglEP0xwA' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1v31SOrGYueeDi2SxOAUuKWqnglEP0xwA" -O ckpt_and_bfm.zip && rm -rf /tmp/cookies.txt
 unzip ckpt_and_bfm.zip
-wget https://drive.google.com/file/d/1Vdlpwghjo4a9rOdn2iJEVlPd0EJegAex/view?usp=sharing -P checkpoints/rs_model/
+cd ..
 
-# cleanup
+mkdir checkpoints
+cd checkpoints
+mkdir rs_model
+cd rs_model
+wget --load-cookies /tmp/cookies.txt "https://drive.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1Vdlpwghjo4a9rOdn2iJEVlPd0EJegAex' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1Vdlpwghjo4a9rOdn2iJEVlPd0EJegAex" -O latest_net_G.pth && rm -rf /tmp/cookies.txt
+cd ../../..
+
+# finish
 cd ..
