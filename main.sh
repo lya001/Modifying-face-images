dfebd743df98c6463d5283b5e82ded7fc9e6c739
yl4519 <yl4519@ic.ac.uk>
2023-01-10 15:54:34 +0000

    implement randr inference

--- main.sh
+++ main.sh
@@ -2,8 +2,20 @@
 
 # Rotate-and-Render
 # 1. move images to 3ddfa/example/Images
+mv $1/* Rotate-and-Render/3ddfa/example/Images
 # 2. generate 3ddfa/example/file_list.txt
-# 3. bash experiments/v100_test.sh
+cd Rotate-and-Render/3ddfa/example/Images
+ls > ../file_list.txt
+cd ../..
+# 3. generate and save 3D parameters
+python inference.py --img_list example/file_list.txt --img_prefix example/Images --save_dir results
+cd ..
+# 4. inference
+bash experiments/v100_test.sh
+cd ..
+# 5. move results to ./randr_results
+mkdir -p randr_results
+mv Rotate-and-Render/results/rs_model/example/orig/* randr_results
 
 # GFPGAN
 cd GFPGAN
