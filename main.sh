edd5adf3c48a2c06134b66193f859365bb6c73ff
yl4519 <yl4519@ic.ac.uk>
2023-04-25 17:45:14 +0100

    implement restoration steps

--- main.sh
+++ main.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
+# GFPGAN
+mkdir -p gfpgan_results1
+cd GFPGAN
+python inference_gfpgan.py -i ../$1 -o ../gfpgan_results1 -v 1.3 -s 2
+cd ..
+
 # Rotate-and-Render
 # 1. copy images to 3ddfa/example/Images
 rm -f Rotate-and-Render/3ddfa/example/Images/*
-cp $1/* Rotate-and-Render/3ddfa/example/Images
+cp gfpgan_results1/restored_faces/* Rotate-and-Render/3ddfa/example/Images
 # 2. generate 3ddfa/example/file_list.txt
 cd Rotate-and-Render/3ddfa/example/Images
 ls > ../file_list.txt
@@ -19,6 +25,24 @@ mkdir -p randr_results
 mv Rotate-and-Render/results/rs_model/example/orig/* randr_results
 
 # GFPGAN
+mkdir -p gfpgan_results2
 cd GFPGAN
-python inference_gfpgan.py -i ../randr_results -o ../output -v 1.3 -s 2
+python inference_gfpgan.py -i ../randr_results -o ../gfpgan_results2 -v 1.3 -s 2
 cd ..
+
+# Facial-Expression-Modifier
+rm -f Facial-Expression-Modifier/input/*
+cp gfpgan_results2/restored_faces/* Facial-Expression-Modifier/input
+cd Facial-Expression-Modifier
+python inference.py
+cd ..
+mkdir -p expr_results
+mv Facial-Expression-Modifier/output/* expr_results
+
+# GFPGAN
+mkdir -p gfpgan_results3
+cd GFPGAN
+python inference_gfpgan.py -i ../expr_results -o ../gfpgan_results3 -v 1.3 -s 2
+cd ..
+mkdir -p output
+cp gfpgan_results3/restored_faces/* output
