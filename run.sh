b8aa50dbfac1028bd99b8f4cc86e465e6c713eef
Yuang Li <yuangli88@hotmail.com>
2023-04-28 15:46:03 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,8 +2,6 @@
 #SBATCH --gres=gpu:1
 #SBATCH --mail-type=ALL # required to send email notifcations
 #SBATCH --mail-user=yl4519 # required to send email notifcations - please replace <your_username> with your college login name or email address
-#export PATH=/vol/bitbucket/yl4519/myenv/bin/:$PATH
-#source activate
 export PATH=/vol/bitbucket/${USER}/miniconda3/bin/:$PATH
 source activate
 #. /vol/cuda/11.0.3-cudnn8.0.5.39/setup.sh
@@ -12,4 +10,4 @@ export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/vol/cuda/10.0.130-cudnn7.6.4.38/targets
 TERM=vt100 # or TERM=xterm
 /usr/bin/nvidia-smi
 
-sh main.sh input
+sh inference.sh input
