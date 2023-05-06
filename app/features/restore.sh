64a285567d809740c3e58af2b73b2347a9756783
yl4519 <yl4519@ic.ac.uk>
2023-05-06 16:38:12 +0100

    return restored image instead of face

--- app/features/restore.sh
+++ app/features/restore.sh
@@ -6,8 +6,8 @@ mkdir -p ./app/input
 cd GFPGAN
 mkdir -p results
 python inference_gfpgan.py -i ../app/input -o ./results -v 1.3 -s 2
-mv -f ./results/restored_faces/*.* ./results/restored_faces/$2
+mv -f ./results/restored_imgs/*.* ./results/restored_imgs/$2
 cd ..
-mv -f ./GFPGAN/results/restored_faces/$2 ./app/results
+mv -f ./GFPGAN/results/restored_imgs/$2 ./app/results
 rm -rf ./app/input
 rm -rf ./GFPGAN/results
